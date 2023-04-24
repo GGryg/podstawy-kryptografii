4b4cb730d92924d9f9630ffdde1612039d4b4903
GGryg <shiryon@wp.pl>
2023-04-24 22:54:53 +0200

    lab3

---
+++ lab3/block.go
@@ -0,0 +1,94 @@
+// Author: Grzegorz Grygorowicz
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"image"
+	"log"
+	"math/rand"
+	"os"
+
+	"golang.org/x/image/bmp"
+)
+
+func main() {
+	inputFile, err := os.Open("images/ga.bmp")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer inputFile.Close()
+
+	inputImage, _, err := image.Decode(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	blockSize := 8
+	imageData := inputImage.(*image.Paletted).Pix
+	size := inputImage.Bounds().Size()
+	keys := getKeys(blockSize)
+
+	ecb(imageData, size, blockSize, keys)
+	cbc(imageData, size, blockSize, keys)
+}
+
+func save(data []byte, path string, message string) {
+	outputFile, err := os.Create(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outputFile.Close()
+
+	//outputImage := image.NewPaletted(image.Rect(0, 0, 800, 800), palette)
+	// outputImage := image.NewGray(image.Rect(0, 0, 800, 800))
+	outputImage := image.NewAlpha(image.Rect(0, 0, 800, 800))
+	copy(outputImage.Pix, data)
+
+	err = bmp.Encode(outputFile, outputImage)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(message)
+}
+
+func ecb(img []byte, size image.Point, blockSize int, keys [][16]byte) {
+	var encoded []byte
+
+	for x := 0; x < size.X; x++ {
+		for y := 0; y < size.Y; y++ {
+			position := x*size.X + y
+			original := img[position]
+
+			new := original ^ keys[x%blockSize][y%blockSize]
+			encoded = append(encoded, new)
+		}
+	}
+
+	save(encoded, "images/e1.bmp", "ECB")
+}
+
+func cbc(img []byte, size image.Point, blockSize int, keys [][16]byte) {
+	var encoded []byte
+	encoded = append(encoded, img[0]^0)
+
+	for i := 1; i < size.X*size.Y; i++ {
+		encoded = append(encoded, encoded[i-1]^img[i]^keys[i%blockSize*128/blockSize][i%blockSize])
+	}
+
+	save(encoded, "images/e2.bmp", "CBC")
+}
+
+func getKeys(blockSize int) [][16]byte {
+	var keys [][16]byte
+
+	for i := 0; i < blockSize; i++ {
+		key := md5.Sum([]byte(fmt.Sprintf("%f", rand.Float64()*float64(i))))
+		//key := sha1.Sum([]byte(fmt.Sprintf("%f", rand.Float64()*float64(i))))
+		//keyStr := hex.EncodeToString(key[:])
+		keys = append(keys, key)
+	}
+
+	return keys
+}
---
+++ lab3/m.py
@@ -0,0 +1,46 @@
+from PIL import Image
+import hashlib
+import random
+
+input_image = Image.open("images/ga.bmp")
+
+
+def save(data, where="images/image.bmp", message="Done"):
+    new_data = bytes(data)
+
+    output_image = input_image.copy()
+    output_image.frombytes(new_data)
+    output_image.save(where)
+    print(message)
+
+
+block_size = 8
+image_data = input_image.tobytes()
+size = input_image.size
+
+# ECB
+new_data = []
+keys = []
+for x in range(block_size):
+    key = hashlib.md5(str(random.random() * x).encode("UTF-8")).digest()
+    keys.append(key)
+
+for x in range(size[0]):
+    for y in range(size[1]):
+        pp = x * size[0] + y
+        op = image_data[pp]
+        pta = op ^ keys[x % block_size][y % block_size]
+        new_data.append(pta)
+
+save(new_data, "images/ecb_crypto.bmp")
+
+
+# CBC
+new_key = 13
+new_data = [image_data[0] ^ new_key]
+for x in range(size[0]*size[1]):
+    new_data.append(new_data[x-1] ^ image_data[x] ^ keys[x%64//8][x%8])
+
+save(new_data, "images/cbc_crypto.bmp")
+
+print("Done")
